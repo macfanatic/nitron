4d0997d440a9a0130f211f5d5a116046a7b6a486
Matt Brewer <matt.brewer@me.com>
2012-06-28 08:28:14 -0400

    Got rid of pesky exception messages that are now being logged as of latest Ruby Motion.

--- lib/nitron/data/relation/finder_methods.rb
+++ lib/nitron/data/relation/finder_methods.rb
@@ -47,7 +47,7 @@ module Nitron
         end
             
         def order(column, opts={})
-          descriptors = sortDescriptors.mutableCopy rescue []
+          descriptors = sortDescriptors.nil? ? [] : sortDescriptors.mutableCopy
           descriptors << NSSortDescriptor.sortDescriptorWithKey(column.to_s, ascending:opts.fetch(:ascending, true))
           self.sortDescriptors = descriptors
           self
